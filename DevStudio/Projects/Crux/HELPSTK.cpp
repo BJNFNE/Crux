9983b6c0a7c8031b3e0bdbcab6fd381b7cbca405
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2024-04-25 16:47:51 +0200

    Create HELPSTK.cpp

---
+++ DevStudio/Projects/Crux/HELPSTK.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+// Initialize help stack
+void hlp_init(int ptr) {
+    printf("Placeholder for hlp_init(%d)\n", ptr);
+}
+
+// Roll
+void roll(int i) {
+    printf("Placeholder for roll(%d)\n", i);
+}
+
+// Remove from help stack
+void hlp_remove(int val) {
+    printf("Placeholder for hlp_remove(%d)\n", val);
+}
+
+// Add to help stack
+void hlp_add(int val) {
+    printf("Placeholder for hlp_add(%d)\n", val);
+}
+
+// Save help stack
+void save_help_stack(int f) {
+    printf("Placeholder for save_help_stack(%d)\n", f);
+}
+
+// Load help stack
+void load_help_stack(int f) {
+    printf("Placeholder for load_help_stack(%d)\n", f);
+}
